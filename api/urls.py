a3aea733909d8d0a7bea9ddd882cafc6cf6fcf44
Morvin-Ian <morvinian@gmail.com>
2022-05-19 16:34:27 +0300

    class-based

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,29 @@
 from django.urls import path
 from . import views
 
+
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import BookList, BookDetail
+
 urlpatterns = [
-    path('book/',views.Booksview, name='books'),
-    path('book/<int:pk>/',views.Bookview, name='book'),
-    path('book/create/',views.BookCreateview, name='book-create'),
-    path('book/update/<int:pk>/',views.BookUpdateview, name='book-update'),
-    path('book/delete/<int:pk>/',views.BookDeleteview, name='book-delete')
+    
+]
+
+
+
+urlpatterns = [
+    # Function based
+    # path('book/',views.Booksview, name='books'),
+    # path('book/<int:pk>/',views.Bookview, name='book'),
+    # path('book/create/',views.BookCreateview, name='book-create'),
+    # path('book/update/<int:pk>/',views.BookUpdateview, name='book-update'),
+    # path('book/delete/<int:pk>/',views.BookDeleteview, name='book-delete'),
+
+
+    # Class based
+    path('book/', BookList.as_view()),
+    path('book/<int:pk>/', BookDetail.as_view()),
 
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -9,60 +9,104 @@ from rest_framework.decorators import api_view
 from rest_framework.authentication import SessionAuthentication,TokenAuthentication,BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-@api_view(["GET"])
-def Booksview(request):
-    try:
+
+# @api_view(["GET"])
+# def Booksview(request):
+#     try:
+#         books = Books.objects.all()
+#         serializer = BookSerializer(books, many=True)
+#         return Response(serializer.data)
+
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)    
+   
+# @api_view(["GET"])
+# def Bookview(request, pk):
+#     try:
+#         book = Books.objects.get(id=pk)
+#         serializer = BookSerializer(book, many=False)
+#         return Response(serializer.data)
+
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)    
+
+ 
+# @api_view(["PUT"])
+# def BookCreateview(request):
+#     try:
+#         serializer = BookSerializer(data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         return Response(serializer.data)
+
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)    
+   
+# @api_view(["GET", "PUT"])
+# def BookUpdateview(request, pk):
+#     try:
+#         book = Books.objects.get(id=pk)
+#         serializer = BookSerializer(instance=book,data = request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#         return Response(serializer.data)
+
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)    
+   
+
+
+# @api_view(["GET", "DELETE"])
+# def BookDeleteview(request, pk):
+#     try:
+#         book = Books.objects.get(id=pk)
+#         book.delete()
+#         return Response("Delete Successful")
+
+#     except Books.DoesNotExist:
+#         return HttpResponse(status=404)    
+   
+class BookList(APIView):
+
+    def get(self, request, format=None):
         books = Books.objects.all()
         serializer = BookSerializer(books, many=True)
         return Response(serializer.data)
 
-    except Books.DoesNotExist:
-        return HttpResponse(status=404)    
-   
-@api_view(["GET"])
-def Bookview(request, pk):
-    try:
-        book = Books.objects.get(id=pk)
-        serializer = BookSerializer(book, many=False)
-        return Response(serializer.data)
-
-    except Books.DoesNotExist:
-        return HttpResponse(status=404)    
-
- 
-@api_view(["PUT"])
-def BookCreateview(request):
-    try:
-        serializer = BookSerializer(data = request.data)
+    def post(self, request, format=None):
+        serializer = BookSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class BookDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Books.objects.get(pk=pk)
+        except Books.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book)
         return Response(serializer.data)
 
-    except Books.DoesNotExist:
-        return HttpResponse(status=404)    
-   
-@api_view(["GET", "PUT"])
-def BookUpdateview(request, pk):
-    try:
-        book = Books.objects.get(id=pk)
-        serializer = BookSerializer(instance=book,data = request.data)
+    def put(self, request, pk, format=None):
+        book = self.get_object(pk)
+        serializer = BookSerializer(book, data=request.data)
         if serializer.is_valid():
             serializer.save()
-        return Response(serializer.data)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    except Books.DoesNotExist:
-        return HttpResponse(status=404)    
-   
-
-
-@api_view(["GET", "DELETE"])
-def BookDeleteview(request, pk):
-    try:
-        book = Books.objects.get(id=pk)
+    def delete(self, request, pk, format=None):
+        book = self.get_object(pk)
         book.delete()
-        return Response("Delete Successful")
-
-    except Books.DoesNotExist:
-        return HttpResponse(status=404)    
-   
+        return Response(status=status.HTTP_204_NO_CONTENT)
