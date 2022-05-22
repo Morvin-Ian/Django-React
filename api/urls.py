b590b60f596f7b88c926730e23af6f5484979f57
Morvin-Ian <morvinian@gmail.com>
2022-05-22 20:48:06 +0300

    finalize

--- api/urls.py
+++ api/urls.py
@@ -13,11 +13,11 @@ urlpatterns = [
 
 urlpatterns = [
     # Function based
-    # path('book/',views.Booksview, name='books'),
-    # path('book/<int:pk>/',views.Bookview, name='book'),
-    # path('book/create/',views.BookCreateview, name='book-create'),
-    # path('book/update/<int:pk>/',views.BookUpdateview, name='book-update'),
-    # path('book/delete/<int:pk>/',views.BookDeleteview, name='book-delete'),
+    path('book/',views.Booksview, name='books'),
+    path('book/<int:pk>/',views.Bookview, name='book'),
+    path('book/create/',views.BookCreateview, name='book-create'),
+    path('book/update/<int:pk>/',views.BookUpdateview, name='book-update'),
+    path('book/delete/<int:pk>/',views.BookDeleteview, name='book-delete'),
 
 
     # Class based
--- api/views.py
+++ api/views.py
@@ -15,61 +15,61 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-# @api_view(["GET"])
-# def Booksview(request):
-#     try:
-#         books = Books.objects.all()
-#         serializer = BookSerializer(books, many=True)
-#         return Response(serializer.data)
+@api_view(["GET"])
+def Booksview(request):
+    try:
+        books = Books.objects.all()
+        serializer = BookSerializer(books, many=True)
+        return Response(serializer.data)
 
-#     except Books.DoesNotExist:
-#         return HttpResponse(status=404)    
+    except Books.DoesNotExist:
+        return HttpResponse(status=404)    
    
-# @api_view(["GET"])
-# def Bookview(request, pk):
-#     try:
-#         book = Books.objects.get(id=pk)
-#         serializer = BookSerializer(book, many=False)
-#         return Response(serializer.data)
+@api_view(["GET"])
+def Bookview(request, pk):
+    try:
+        book = Books.objects.get(id=pk)
+        serializer = BookSerializer(book, many=False)
+        return Response(serializer.data)
 
-#     except Books.DoesNotExist:
-#         return HttpResponse(status=404)    
+    except Books.DoesNotExist:
+        return HttpResponse(status=404)    
 
  
-# @api_view(["PUT"])
-# def BookCreateview(request):
-#     try:
-#         serializer = BookSerializer(data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#         return Response(serializer.data)
+@api_view(["GET","POST"])
+def BookCreateview(request):
+    try:
+        serializer = BookSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
 
-#     except Books.DoesNotExist:
-#         return HttpResponse(status=404)    
+    except Books.DoesNotExist:
+        return HttpResponse(status=404)    
    
-# @api_view(["GET", "PUT"])
-# def BookUpdateview(request, pk):
-#     try:
-#         book = Books.objects.get(id=pk)
-#         serializer = BookSerializer(instance=book,data = request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#         return Response(serializer.data)
+@api_view(["GET", "PUT"])
+def BookUpdateview(request, pk):
+    try:
+        book = Books.objects.get(id=pk)
+        serializer = BookSerializer(instance=book,data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+        return Response(serializer.data)
 
-#     except Books.DoesNotExist:
-#         return HttpResponse(status=404)    
+    except Books.DoesNotExist:
+        return HttpResponse(status=404)    
    
 
 
-# @api_view(["GET", "DELETE"])
-# def BookDeleteview(request, pk):
-#     try:
-#         book = Books.objects.get(id=pk)
-#         book.delete()
-#         return Response("Delete Successful")
+@api_view(["GET", "DELETE"])
+def BookDeleteview(request, pk):
+    try:
+        book = Books.objects.get(id=pk)
+        book.delete()
+        return Response("Delete Successful")
 
-#     except Books.DoesNotExist:
-#         return HttpResponse(status=404)    
+    except Books.DoesNotExist:
+        return HttpResponse(status=404)    
    
 class BookList(APIView):
 
